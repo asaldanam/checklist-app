476c3245d1fc2b8f02156096a97dda69fa2cbbbd
asaldana <abel.saldana.martinez@everis.com>
2018-11-08 00:16:41 +0100

    delete animation

--- src/shared/ListItem.js
+++ src/shared/ListItem.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import fs from '../firestore';
+import Swipe from 'react-easy-swipe';
 
 class ListItem extends Component {
 
   constructor() {
     super();
     this.state = {
-      checkState: ''
+      checkState: '',
     };
   };
   componentDidMount() {
     this.setState({
-      checkState: this.props.checkState
+      checkState: this.props.checkState,
+      deleted: false,
+      swipePosition: 0,
+      swipeEnabled: true,
+      swipeAnimation: false
     })
   }
 
@@ -21,12 +26,44 @@ class ListItem extends Component {
       .update({'checked': !this.props.productData.checked})
   }
 
+  handleSwipeStart(event) {
+    // this.setState({ 
+    //   swipeAnimation: false
+    // });   
+  }
+
+  handleSwipeEnd(event) {
+    this.setState({  swipeEnabled: false, swipeAnimation: true,});
+    this.state.swipePosition < -100 ? 
+      this.setState({swipePosition: -window.innerWidth, deleted: true})
+      : this.setState({swipePosition: 0,})
+
+    setTimeout(() => {this.setState({ 
+      swipeEnabled: true, 
+      swipeAnimation: false,
+    });}, 500 );
+  }
+
+  handleSwipeMove(position, event) {
+    if (position.x < 0 && this.state.swipeEnabled) {
+      this.setState({swipePosition: position.x})
+    }
+    console.log(this.state.swipePosition); 
+  }
+
   render() {
     return ( 
-      <div
+      <Swipe
+      onSwipeEnd={this.handleSwipeEnd.bind(this)}
+      onSwipeStart={this.handleSwipeStart.bind(this)}
+      onSwipeMove={this.handleSwipeMove.bind(this)}
       onClick={() => this.handleCheck()}
-      className={ `c-listitem --${this.props.productData.checked ? 'checked-opacity' : 'list'} `} 
+      className={ `c-listitem --${this.props.productData.checked ? 'checked-opacity' : 'list'} --${this.state.deleted ? 'deleted' : ''} `} 
       >
+      <div
+        className={`c-listitem-container ${this.state.swipeAnimation ? '--animation' : ''}`}
+        style={{transform: `translateX(${this.state.swipePosition}px)`}}>
+
         <div className="c-listitem-stateicon">
           <div className="a"></div>
           <div className="b"></div>
@@ -35,6 +72,7 @@ class ListItem extends Component {
           <span> {this.props.productData.name} </span>
         </div>
       </div>
+      </Swipe>
     );
   }
 }
